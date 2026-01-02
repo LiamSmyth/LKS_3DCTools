2118c79d3c2994f8fbed7df1b8f749b1f5a0e89f
LiamSmyth <wendallhitherd@gmail.com>
2026-01-02 12:24:53 -0800

    rename scripts

--- UserProjects/toggle_16x_decimate_proxy.py
+++
@@ -1,9 +0,0 @@
-# This is just a wrapper to always cache to 16x decimation
-import coat
-
-
-coat.ui.cmd("$Decimate16X")
-coat.ui.cmd("$ToggleCachingVolume")
-
-# Show summary message to user
-coat.ui.showInfoMessage("16x decimation proxy toggled", 3000)
